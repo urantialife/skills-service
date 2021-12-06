b87ea69fa80aec3ca498ba90f193e013b4f3c46f
mosefer <cjmoses@evoforge.org>
2021-12-06 12:27:53 -0500

    #889 make certain that readOnly/copiedFrom/copiedFromProjectId attribtues are included on subject/skills endpoint results

--- service/src/main/java/skills/services/admin/SkillsAdminService.groovy
+++ service/src/main/java/skills/services/admin/SkillsAdminService.groovy
@@ -645,6 +645,9 @@ class SkillsAdminService {
                 selfReportingType: partial.getSelfReportingType(),
                 numSkillsRequired: partial.getNumSkillsRequired(),
                 enabled: Boolean.valueOf(partial.enabled),
+                readOnly: partial.readOnly(),
+                copiedFrom: partial.copiedFrom,
+                copiedFromProjectId: partial.copiedFromProjectId
         )
 
         if (partial.skillType == SkillDef.ContainerType.Skill) {
