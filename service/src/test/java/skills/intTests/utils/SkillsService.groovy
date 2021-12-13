e5a703225642b2bb7802475051f33c2a51cb272d
mosefer <cjmoses@evoforge.org>
2021-12-13 14:01:46 -0500

    #889 integration tests wip

--- service/src/test/java/skills/intTests/utils/SkillsService.groovy
+++ service/src/test/java/skills/intTests/utils/SkillsService.groovy
@@ -1223,7 +1223,7 @@ class SkillsService {
     }
 
     def importSkillFromCatalog(String importIntoProjectId, String importIntoSubjectId, String catalogSkillProjectId, String catalogSkillSkillId) {
-        return wsHelper.adminPost("/projects/${importIntoProjectId}/subjects/${importIntoSubjectId}/import/${catalogSkillProjectId}/${catalogSkillSkillId}")
+        return wsHelper.adminPost("/projects/${importIntoProjectId}/subjects/${importIntoSubjectId}/import/${catalogSkillProjectId}/${catalogSkillSkillId}", [:])
     }
 
     def bulkExportSkillsToCatalog(String projectId, List<String> skillIds) {
